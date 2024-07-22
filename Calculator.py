c730e46c6dbb09218e8831369e813d893ba7e092
Nirdosh2004 <nirdoshkushwaha75@gmail.com>
2024-07-22 17:28:40 +0530

    first commit

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+# x = 1
+# y = 3
+# z = x+y
+# print(z)
+
+# x = input("Enter the first number : ")
+# y = input("Enter the second operator : ")
+
+# z = int(x) / int(y)
+# print(f"{z:.5f}")
+# def main():
+#           name = input("What's your name : ?\n")
+#           hello()
+
+
+# def hello():
+#         print("hello" , name)
+
+# main()
+
+def main():
+          x = int(input("what's x : "))
+          print("x squared is : " ,square(x))
+
+def square(n):
+        return n*n
+
+main()
---
+++ First_Lecture.py
@@ -0,0 +1,21 @@
+# # print("Hello, World")
+# # print('hello world')
+
+# input_name = input("What is your name? ").strip().title()
+
+# first , last = input_name.split()
+
+# print(first)
+# print(last)
+
+# # print("Hello," , input_name , sep="")
+
+
+# # print("Hello, \'Friend\'")
+# # input_name = input_name   # Remove the irrilivent whitespaces from the right and left the input 
+# # input_name =  input_name.title()  # it capitalize all the letters first word
+
+# # print(f"Hello, {input_name}")
+
+
+
